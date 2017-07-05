827d78fa3d5745cb2b23aa1a56a738e56f46099b
bokuweb <bokuweb@users.noreply.github.com>
2017-07-05 12:28:08 +0900

    Update renderer.test.js

--- test/renderer/renderer.test.js
+++ test/renderer/renderer.test.js
@@ -5,7 +5,7 @@ import { render } from 'react-dom';
 import { screenshot } from '../../renderer/test-helper';
 
 test('react', async t => {
-	render(<div>Hello, world</div>, document.body)
+	render(<div>Hello, world..!?</div>, document.body)
 	document.body.style.backgroundColor = "#ccc";
 	t.is(1, 1);
 	await screenshot('screenshots/hello_react.png');
