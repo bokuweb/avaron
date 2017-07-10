1b6eb4575d69c145789c48bb83f20fc5ec78cdeb
bokuweb <bokuweb12@gmail.com>
2017-07-10 17:21:17 +0900

    Update readme

--- test/common/snapshot.test.js
+++ test/common/snapshot.test.js
@@ -3,8 +3,9 @@ import React from 'react';
 import render from 'react-test-renderer';
 
 const HelloWorld = () => <h1>Hello World.....!</h1>;
+const HelloReact = (props) => <div><HelloWorld />{props.children}</div>;
 
 test('HelloWorld component', t => {
-	const tree = render.create(<HelloWorld />).toJSON();
+	const tree = render.create(<HelloReact><HelloWorld /></HelloReact>).toJSON();
 	t.snapshot(tree);
 });
